d19dd89b83b0862faa1fd0ab95668aaa4b877764
Vlad <vladislav.tihonov@innowise.com>
2025-05-09 13:04:50 +0300

    add: ValidationPipe

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,4 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsString } from "class-validator";
 
 export class CreatePostDto {
     @ApiProperty({
@@ -6,17 +7,21 @@ export class CreatePostDto {
         type: String,
         example: 'House of dragon'
     })
+    @IsString()
+    @IsNotEmpty()
     title: string;
     @ApiProperty({
         description:'content',
         type: Object,
         example: 'group of pictures'
     })
+    @IsNotEmpty()
     content: any;
     @ApiProperty({
         description:'author',
         type: Object,
         example: 'Name Sn. Ln.'
     })
+    @IsNotEmpty()
     author: any;
 }
